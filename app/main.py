e08c8e332048ecdcda8d6ba767e62d47a8d5033e
Buddhadeb Das <40224680+dev48192@users.noreply.github.com>
2025-05-05 12:50:35 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,18 @@ from app.routes import example
 
 app = FastAPI()
 
+origins = [
+    "https://ebc-app-9e775.web.app/",         # Firebase default domain
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # or limit to ["GET", "POST"] for more security
+    allow_headers=["*"],
+)
+
 app.include_router(example.router)
 
 @app.get("/")
